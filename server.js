aed44bc7a0ae933b733e82d1c83c77f1dde20919
Viktor <belinchio@yandex.ru>
2024-12-22 14:04:37 +0300

    исправленна обраюотка ошибок

--- server.js
+++ server.js
@@ -8,44 +8,44 @@ server.on("connection", connection => {
     try {
         console.log("Новое подключение");
         connection.add(connection);
+
+        // Отправка приветственного сообщение
+        connection.send(JSON.stringify({
+            type: "system",
+            text: "Добро пожаловать в чат!",
+        }));
+
+        // Обработка входящих сообщений от клиента
+        connection.on("message", message => {
+            try {
+                const data = JSON.parse(message);
+                console.log(`Сщщбщение от ${data.username}: ${data.text}`);
+                clients.forEach(client => {
+                    if(client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: "message",
+                            username: data.username,
+                            text: data.text,
+                        }));
+                    }
+                });
+            } catch (err) {
+                console.error("Ошибка обработкт сообщения: ", err);
+            }
+        });
+
+        //Обработка отключения клиента
+        connection.on("close", () => {
+            try {
+                console.log("Клиент отключился");
+                clients.delete(connection);
+            } catch (err) {
+                console.error(`Ошибка обработки данных: ${err}`);
+            }
+        });
     } catch (err) {
         console.error(`Ошибка подключения: ${err}`);
     }
-
-    // Отправка приветственного сообщение
-    connection.send(JSON.stringify({
-        type: "system",
-        text: "Добро пожаловать в чат!",
-    }));
-
-    // Обработка входящих сообщений от клиента
-    connection.on("message", message => {
-        try {
-            const data = JSON.parse(message);
-            console.log(`Сщщбщение от ${data.username}: ${data.text}`);
-            clients.forEach(client => {
-                if(client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({
-                        type: "message",
-                        username: data.username,
-                        text: data.text,
-                    }));
-                }
-            });
-        } catch (err) {
-            console.error("Ошибка обработкт сообщения: ", err);
-        }
-    });
-
-    //Обработка отключения клиента
-    connection.on("close", () => {
-        try {
-            console.log("Клиент отключился");
-            clients.delete(connection);
-        } catch (err) {
-            console.error(`Ошибка обработки данных: ${err}`);
-        }
-    });
 });
 
 console.log("Сервер запущен на порту: 3000");
